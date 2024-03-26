760b54537c97686cf51884d793a219d9f6f3ec2c
Lionel Faure <faure_lionel@hotmail.fr>
2024-03-26 17:58:24 +0100

    Slider, rectification des dots pour qu'ils soit checked.

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -1,15 +1,16 @@
-import { useEffect, useState } from "react";
-import { useData } from "../../contexts/DataContext";
-import { getMonth } from "../../helpers/Date";
+import { useEffect, useState } from 'react'
+import { useData } from '../../contexts/DataContext'
+import { getMonth } from '../../helpers/Date'
 
-import "./style.scss";
+import './style.scss'
+
 
 const Slider = () => {
-  const { data } = useData();
-  const [index, setIndex] = useState(0);
-  const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) - new Date(evtB.date)
-  );
+  const { data } = useData()
+  const [index, setIndex] = useState(0)
+  const byDateDesc = data?.focus.sort(
+    (evtA, evtB) => new Date(evtA.date) > new Date(evtB.date) ? 1 : -1
+  )
   useEffect(() => {
     const nextCard = setTimeout(
       () => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0),
@@ -18,32 +19,34 @@ const Slider = () => {
     return () => clearTimeout(nextCard)
   }, [index, byDateDesc])
   return (
-    <div className="SlideCardList">
+    <div className='SlideCardList'>
       {byDateDesc?.map((event, idx) => (
         <>
           <div
             key={event.id}
             className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
+              index === idx ? 'display' : 'hide'
             }`}
           >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
+            <img src={event.cover} alt='forum' />
+            <div className='SlideCard__descriptionContainer'>
+              <div className='SlideCard__description'>
                 <h3>{event.title}</h3>
                 <p>{event.description}</p>
                 <div>{getMonth(new Date(event.date))}</div>
               </div>
             </div>
           </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
+          <div className='SlideCard__paginationContainer'>
+            <div className='SlideCard__pagination'>
               {byDateDesc.map((_, radioIdx) => (
                 <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={idx === radioIdx}
+                  key={`${event.date}`}
+                  type='radio'
+                  name='radio-button'
+                  checked={index === radioIdx}
+                  
+                  readOnly
                 />
               ))}
             </div>
@@ -51,7 +54,7 @@ const Slider = () => {
         </>
       ))}
     </div>
-  );
-};
+  )
+}
 
-export default Slider;
+export default Slider
