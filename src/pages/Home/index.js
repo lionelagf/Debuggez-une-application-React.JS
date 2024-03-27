f8975e2d40acc810b1d3f76a1ff62550ed74b9db
Lionel Faure <faure_lionel@hotmail.fr>
2024-03-27 15:08:53 +0100

    Home, ajout des id sur les section pour la navigation navbar.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,22 +1,20 @@
-import Menu from "../../containers/Menu";
-import ServiceCard from "../../components/ServiceCard";
-import EventCard from "../../components/EventCard";
-import PeopleCard from "../../components/PeopleCard";
+import Menu from '../../containers/Menu'
+import ServiceCard from '../../components/ServiceCard'
+import EventCard from '../../components/EventCard'
+import PeopleCard from '../../components/PeopleCard'
 
-import "./style.scss";
-import EventList from "../../containers/Events";
-import Slider from "../../containers/Slider";
-import Logo from "../../components/Logo";
-import Icon from "../../components/Icon";
-import Form from "../../containers/Form";
-import Modal from "../../containers/Modal";
-import { useData } from "../../contexts/DataContext";
+import './style.scss'
+import EventList from '../../containers/Events'
+import Slider from '../../containers/Slider'
+import Logo from '../../components/Logo'
+import Icon from '../../components/Icon'
+import Form from '../../containers/Form'
+import Modal from '../../containers/Modal'
+import { useData } from '../../contexts/DataContext'
 
 const Page = () => {
   const { data } = useData()
-    const last = data?.events?.[data.events.length - 1]
-        
-  
+const last = data?.events?.[data.events.length - 1]
   return (
     <>
       <header>
@@ -26,7 +24,7 @@ const Page = () => {
         <section className='SliderContainer'>
           <Slider />
         </section>
-        <section className='ServicesContainer'>
+        <section id='nos-services' className='ServicesContainer'>
           <h2 className='Title'>Nos services</h2>
           <p>Nous organisons des événements sur mesure partout dans le monde</p>
           <div className='ListContainer'>
@@ -55,11 +53,11 @@ const Page = () => {
             </ServiceCard>
           </div>
         </section>
-        <section className='EventsContainer'>
+        <section id='nos-realisations' className='EventsContainer'>
           <h2 className='Title'>Nos réalisations</h2>
           <EventList />
         </section>
-        <section className='PeoplesContainer'>
+        <section id='notre-equipe' className='PeoplesContainer'>
           <h2 className='Title'>Notre équipe</h2>
           <p>Une équipe d’experts dédiés à l’ogranisation de vos événements</p>
           <div className='ListContainer'>
@@ -119,6 +117,7 @@ const Page = () => {
           <h3>Notre derniére prestation</h3>
           <EventCard
             imageSrc={last?.cover}
+            imageAlt={last?.title}
             title={last?.title}
             date={new Date(last?.date)}
             small
@@ -159,4 +158,4 @@ const Page = () => {
   )
 }
 
-export default Page;
+export default Page
